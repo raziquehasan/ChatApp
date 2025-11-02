25c5a6e81c06c6f2294bd221a7f4c188ee4137d1
raziquehasan <hasanrazique@gmail.com>
2025-11-02 21:50:04 +0530

    bhaii

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,10 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthContextProvider } from './context/AuthContext.jsx'
 import { SocketContextProvider } from './context/SocketContext.jsx'
+import axios from 'axios'
+
+// Set axios defaults to include cookies
+axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
