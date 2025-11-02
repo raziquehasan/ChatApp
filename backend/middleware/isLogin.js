16b0034d4ee3e9991d7e4ef98f3b793043b910e3
raziquehasan <hasanrazique@gmail.com>
2025-11-02 21:40:32 +0530

    add

--- backend/middleware/isLogin.js
+++ backend/middleware/isLogin.js
@@ -1,15 +1,15 @@
  import jwt from 'jsonwebtoken'
 import User from '../Models/userModels.js'
 
-const isLogin = (req, res, next) => {
+const isLogin = async (req, res, next) => {
     try {
         const token = req.cookies.jwt
         if (!token) return res.status(500).send({ success: false, message: "User Unauthorize" });
         const decode = jwt.verify(token,process.env.JWT_SECRET);
         if(!decode)  return res.status(500).send({success:false, message:"User Unauthorize -Invalid Token"})
-        const user = User.findById(decode.userId).select("-password");
+        const user = await User.findById(decode.userId).select("-password");
         if(!user) return res.status(500).send({success:false, message:"User not found"})
-        req.user = user,
+        req.user = decode;
         next()
     } catch (error) {
         console.log(`error in isLogin middleware ${error.message}`);
